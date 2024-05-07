96b895f0dbc9a86fa529e628c627daf83ebb2395
Sri Sreedhar <srisreedhar@live.com>
2024-05-07 15:59:11 +0530

    input() , int(), print()

---
+++ interview_2.py
@@ -0,0 +1,18 @@
+# candidate Interview
+# wap to collect the name,age,education,area,college name, phonenumber
+# of the people who attend the interview and print
+
+
+name = input("Enter your name here : ")
+education = input("Enter your degree here : ")
+college_name = input("Enter your college name here : ")
+phone_number = input("Enter your Phone number here : ")
+current_year = 2024
+birth_year = input("Enter the year you're born : ")
+
+age = current_year - birth_year
+
+print("""
+Hi,
+Below are the details you have entered:
+name :""",name )
