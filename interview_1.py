21fe1c19812b983fed9b8457e1ca410425593559
Sri Sreedhar <srisreedhar@live.com>
2024-05-07 15:58:32 +0530

    Our First Program

---
+++ interview_1.py
@@ -0,0 +1,18 @@
+# candidate Interview
+# wap to collect the name,age,education,area,college name, phonenumber
+# of the people who attend the interview and print
+
+
+name ="sphoorthi"
+education = "bsc"
+college_name = "badruka college"
+phone_number = 123456789
+current_year = 2024
+birth_year = 2000
+
+age = current_year - birth_year
+
+print("""
+Hi,
+Below are the details you have entered:
+name :""",name )
